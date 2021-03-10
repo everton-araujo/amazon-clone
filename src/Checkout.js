0d8b23364d34854a4a19b2f58af856c52b0f281c
Everton <everton.aaraujo@outlook.com>
2021-03-09 21:26:37 -0300

    displaying email on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,7 +7,7 @@ import { useStateValue } from './StateProvider';
 import './Checkout.css';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -19,6 +19,7 @@ function Checkout() {
         />
 
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout_title">
             Your shopping Basket
           </h2>
